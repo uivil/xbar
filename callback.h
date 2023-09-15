b6950cb4461327acbe508610950a2869bee55105
Liviu Solcovenco <solcovenco.liviu@gmail.com>
2023-09-15 18:24:36 +0200

    Added ALSA and the Weather block

--- callback.h
+++ callback.h
@@ -1,4 +1,8 @@
 #include "wifi-scan/wifi_scan.h"
+#include <curl/curl.h>
+
+#define MIN(a, b) (a) > (b) ? (b) : (a)
+#define MAX(a, b) (a) > (b) ? (a) : (b)
 
 const char itos[] = {"00010203040506070809"
                      "10111213141516171819"
@@ -9,7 +13,7 @@ const char itos[] = {"00010203040506070809"
                      "60616263646566676869"
                      "70717273747576777879"
                      "80818283848586878889"
-                     "90919293949596979899"};
+                     "90919293949596979899100"};
 
 void *battery_init() {
   static char d[256];
@@ -70,6 +74,71 @@ void datetime(void *p, char *s) {
   memcpy(s, itos + timeinfo->tm_min * 2, 2);
 }
 
+struct alsa_cb {
+  snd_mixer_selem_id_t *sid;
+  snd_mixer_t *handle;
+  snd_mixer_elem_t *elem;
+  long volume, minvol, maxvol;
+};
+
+void alsa_cpy(char *s, long vol) {
+  static char icon[] = {0xf0, 0x9f, 0x94, 0x88};
+  memcpy(s, icon, 4);
+  s += 5;
+  s[2] = ' ';
+  memcpy(s, itos + vol * 2, 2 + (vol > 99));
+}
+
+void alsa(void *p, char *s) {
+  static char icon[] = {0xf0, 0x9f, 0x94, 0x88};
+  struct alsa_cb *cb = p;
+  long vol;
+
+  snd_mixer_selem_get_playback_volume(cb->elem, 0, &cb->volume);
+  vol = cb->volume * 100 / (cb->maxvol - cb->minvol);
+  alsa_cpy(s, vol);
+}
+
+int alsa_click(void *p, char *s, int btn) {
+  struct alsa_cb *cb = p;
+  int inc[2] = {3, -3};
+  long vol;
+
+  cb->volume += inc[btn];
+  cb->volume = MAX(cb->volume, cb->minvol);
+  cb->volume = MIN(cb->volume, cb->maxvol);
+  snd_mixer_selem_set_playback_volume_all(cb->elem, cb->volume);
+  vol = cb->volume * 100 / (cb->maxvol - cb->minvol);
+  alsa_cpy(s, vol);
+
+  return 1;
+}
+
+void *alsa_init() {
+  struct alsa_cb *cb = malloc(sizeof(*cb));
+  const char *card = "default";
+  const char *selem_name = "Master";
+
+  snd_mixer_open(&cb->handle, 0);
+  snd_mixer_attach(cb->handle, card);
+  snd_mixer_selem_register(cb->handle, NULL, NULL);
+  snd_mixer_load(cb->handle);
+
+  snd_mixer_selem_id_malloc(&cb->sid);
+  snd_mixer_selem_id_set_index(cb->sid, 0);
+  snd_mixer_selem_id_set_name(cb->sid, selem_name);
+  cb->elem = snd_mixer_find_selem(cb->handle, cb->sid);
+  snd_mixer_selem_get_playback_volume_range(cb->elem, &cb->minvol, &cb->maxvol);
+  return cb;
+}
+
+void alsa_close(void *p) {
+  struct alsa_cb *cb = p;
+  snd_mixer_selem_id_free(cb->sid);
+  snd_mixer_close(cb->handle);
+  free(cb);
+}
+
 void *cpu_init() {
   int fd;
   fd = open("/proc/stat", O_RDONLY);
@@ -98,6 +167,7 @@ void cpu(void *p, char *s) {
   lseek(fd, off, SEEK_SET);
 
   k = get_nprocs();
+  // padding 1 space
   s += 1;
   while (k--) {
     n = read(fd, buff, 96);
@@ -126,9 +196,10 @@ void cpu(void *p, char *s) {
   lseek(fd, 0, SEEK_SET);
 }
 
-void cpu_click(void *p, char *s, int btn) {
+int cpu_click(void *p, char *s, int btn) {
   //  if (!fork())
   //    execl("/bin/xterm", "/bin/xterm", "-s", "htop", NULL);
+  return 0;
 }
 
 void cpu_close(void *p) {
@@ -174,4 +245,68 @@ void temp_close(void *p) {
 void *temp_init() {
   int fd = open("/proc/acpi/ibm/thermal", O_RDONLY);
   return (void *)fd;
+}
+
+// void *wireless_init() {
+//   struct wifi_scan *wf;
+//   wf = wifi_scan_init("wlan0");
+//
+//   return wf;
+// }
+//
+// void wireless(void *p, char *s) {
+//   return;
+//   struct wifi_scan *wf = p;
+//   struct station_info si;
+// #define BSS_INFOS 10
+//	struct bss_info bss[BSS_INFOS];
+//   int status = wifi_scan_all(wf, bss, BSS_INFOS);
+//   printf("%d\n", status);
+//   if (status < 0) return;
+//
+//   for (int i = 0; i < status && i < BSS_INFOS; i++) {
+//     if (bss[i].status == BSS_ASSOCIATED)
+//       printf("%s\n", bss[i].ssid);
+//   }
+//
+// }
+//
+// void wireless_close(void *p) { wifi_scan_close(p); }
+
+struct weather_cb {
+  CURL *curl;
+  char data[1 << 10];
+};
+
+size_t weather_writefunc(char *ptr, size_t size, size_t nmemb, void *userdata) {
+  char *s = userdata;
+  memcpy(s, ptr, nmemb);
+  return nmemb * size;
+}
+
+void *weather_init() {
+  static char *url = "https://wttr.in/?T&format=%c%f%20%w";
+  CURLcode res;
+  struct weather_cb *cb = malloc(sizeof(*cb));
+
+  cb->curl = curl_easy_init();
+  if (!cb->curl)
+    return 0;
+  curl_easy_setopt(cb->curl, CURLOPT_URL, url);
+  curl_easy_setopt(cb->curl, CURLOPT_WRITEFUNCTION, weather_writefunc);
+  curl_easy_setopt(cb->curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+  return cb;
+}
+
+void weather(void *p, char *s) {
+  struct weather_cb *cb = p;
+  curl_easy_setopt(cb->curl, CURLOPT_WRITEDATA, s);
+  CURLcode res;
+  res = curl_easy_perform(cb->curl);
+}
+
+void weather_close(void *p) {
+  struct weather_cb *cb = p;
+  curl_easy_cleanup(cb->curl);
+  free(cb);
 }
--- config.h
+++ config.h
@@ -4,17 +4,19 @@
 
 #include "callback.h"
 
-#define NUMBLOCKS 4
+#define NUMBLOCKS 6
 const char *fontname = "monospace:size=10";
 char *const delimiter = "\xe2\x96\x95";
 char barheight = 20; // px
 
-interval_func interval_clbk[] = { temp, cpu, battery, datetime};
-click_func click_clbk[] = { 0, cpu_click, 0, 0};
-init_func init_clbk[] = {temp_init, cpu_init, battery_init, 0};
-close_func close_clbk[] = {temp_close, cpu_close, 0, 0};
-int interval[] = {1, 1, 2, 10};
+interval_func interval_clbk[] = {weather, alsa, temp, cpu, battery, datetime};
+click_func click_clbk[] = {0, alsa_click, 0, cpu_click, 0, 0};
+init_func init_clbk[] = {weather_init, alsa_init,    temp_init,
+                         cpu_init,     battery_init, 0};
+close_func close_clbk[] = {weather_close, alsa_close, temp_close,
+                           cpu_close,     0,          0};
+int interval[] = {60 * 60 * 2, 1, 1, 1, 2, 10};
 void *user_ptr[NUMBLOCKS];
-int block_size[] = {9, 25, 10, 7};
+int block_size[] = {24, 8, 9, 25, 10, 7};
 
 #endif
